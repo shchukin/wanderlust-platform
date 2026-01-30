9ff738169847e11cb2423e074c1e1b138f6929cd
Anton Shchukin <a.a.shchukin@gmail.com>
2026-01-30 21:44:33 +0700

    build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -89,7 +89,7 @@ document.addEventListener('DOMContentLoaded', () => {
             return;
         }
 
-        if (event.target.closest('.user__close .button')) {
+        if (event.target.closest('.user__dropdown .menu__close .button')) {
             event.preventDefault();
             event.target.closest('.user').classList.remove('user--expanded');
             return;
@@ -108,6 +108,40 @@ document.addEventListener('DOMContentLoaded', () => {
             const $user = document.querySelectorAll('.user--expanded');
             $user.forEach((user) => {
                 user.classList.remove('user--expanded');
+            });
+        }
+    });
+
+
+    /* Toggle user dropdown */
+
+    document.addEventListener('click', (event) => {
+
+        if (event.target.closest('.search__handler')) {
+            event.preventDefault();
+            event.target.closest('.search__filter').classList.toggle('search__filter--expanded');
+            return;
+        }
+
+        if (event.target.closest('.search__dropdown .menu__close .button')) {
+            event.preventDefault();
+            event.target.closest('.search__filter').classList.remove('search__filter--expanded');
+            return;
+        }
+
+        /* Close by clicking outside */
+        if (!event.target.closest('.search__filter')) {
+            const $search = document.querySelector('.search__filter--expanded');
+            $search?.classList.remove('search__filter--expanded');
+        }
+    });
+
+    /* Close by Esc */
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            const $search = document.querySelectorAll('.search__filter--expanded');
+            $search.forEach((search) => {
+                search.classList.remove('search__filter--expanded');
             });
         }
     });
