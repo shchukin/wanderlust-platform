41107f49f57bd5aead9d6be6a3b7b30632c3ca22
Anton Shchukin <a.a.shchukin@gmail.com>
2026-02-05 10:19:59 +0700

    input

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -205,4 +205,46 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', togglePageScrollClass, { passive: true });
 
 
+    /* Input error */
+    document.addEventListener('focusin', (event) => {
+        const $input = event.target.closest('.input');
+        if (!$input) return;
+        $input.classList.remove('input--error');
+        let $sibling = $input.nextElementSibling;
+        while ($sibling && $sibling.classList.contains('helper')) {
+            $sibling.classList.remove('helper--error');
+            $sibling.hidden = true;
+            $sibling = $sibling.nextElementSibling;
+        }
+    });
+
+    /* Select placeholder */
+    const selectPlaceholder = ($element) => {
+        const $parent = $element.closest('.input');
+        if (!$parent) return;
+        $parent.classList.toggle('input--placeholder-is-chosen', $element.value === 'placeholder');
+    };
+
+    document.querySelectorAll('select.input__widget').forEach(($element) => {
+        selectPlaceholder($element);
+        $element.addEventListener('change', () => {
+            selectPlaceholder($element);
+        });
+    });
+
+    /* Expanding textarea */
+    const expandTextarea = ($element) => {
+        $element.style.height = 'auto';
+        const computedStyles = window.getComputedStyle($element);
+        const borderWidth = parseInt(computedStyles.borderWidth, 10) || 0;
+        $element.style.height = `${$element.scrollHeight + 2 * borderWidth}px`;
+    };
+
+    document.querySelectorAll('.input--expandable .input__widget').forEach(($element) => {
+        expandTextarea($element);
+        $element.addEventListener('input', () => {
+            expandTextarea($element);
+        });
+    });
+
 });
