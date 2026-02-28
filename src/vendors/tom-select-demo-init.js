40b24a620d97834caedb37e249757563b339b2aa
Anton Shchukin <a.a.shchukin@gmail.com>
2026-02-28 12:04:01 +0700

    selectize second drfat

--- src/vendors/tom-select-demo-init.js
+++ src/vendors/tom-select-demo-init.js
@@ -1,14 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const selectElement = document.querySelector('.js-select');
-
-    if (selectElement && window.TomSelect) {
-        new TomSelect(selectElement, {
-            create: true,
-            sortField: {
-                field: 'text',
-                order: 'asc'
-            },
-            placeholder: selectElement.getAttribute('placeholder') || 'Select an option...'
-        });
+    if (!window.TomSelect) {
+        return;
     }
+
+    document.querySelectorAll('.js-select-multi').forEach((selectElement) => {
+        new TomSelect(selectElement, {
+            hidePlaceholder: false,
+            maxItems: 3,
+            plugins: ['remove_button'],
+            placeholder: selectElement.getAttribute('placeholder') || 'Pick up to 3 options'
+        });
+    });
+
+    document.querySelectorAll('.js-select-multi-static').forEach((selectElement) => {
+        new TomSelect(selectElement, {
+            allowEmptyOption: true,
+            create: false,
+            maxItems: 3,
+            plugins: ['remove_button'],
+            placeholder: selectElement.getAttribute('placeholder') || 'Pick up to 3 options'
+        });
+    });
+
+    document.querySelectorAll('.js-select-tags').forEach((inputElement) => {
+        new TomSelect(inputElement, {
+            create: true,
+            persist: false,
+            plugins: ['remove_button'],
+            placeholder: inputElement.getAttribute('placeholder') || 'Add tags'
+        });
+    });
 });
