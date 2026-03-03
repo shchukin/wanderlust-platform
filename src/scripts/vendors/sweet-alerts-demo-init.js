47e4c8da413554926c0ac13c7c9acbeaccf40b0e
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-03 19:21:39 +0700

    sweet alert

--- src/scripts/vendors/sweet-alerts-demo-init.js
+++ src/scripts/vendors/sweet-alerts-demo-init.js
@@ -1,30 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
+import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/+esm';
 
-    if (!window.Swal) {
-        return;
-    }
-
-    const MySwal = Swal.mixin({
-        customClass: {
-            confirmButton: 'button button--height-44px',
-            cancelButton: 'button button--height-44px button--light-gray-outline',
-            denyButton: 'button button--height-44px button--red',
-            title: 'title title--24px'
-        },
-        buttonsStyling: false
-    });
-
-    const alertBtn = document.querySelector('#sweet-alert-demo');
-    if (alertBtn) {
-        alertBtn.addEventListener('click', () => {
-            MySwal.fire({
-                title: "Do you want to save the changes?",
-                icon: "question",
-                showCancelButton: true,
-                confirmButtonText: "Save",
-                denyButtonText: "Don't save",
-                cancelButtonText: "Cancel"
-            });
-        });
-    }
+const MySwal = Swal.mixin({
+    customClass: {
+        confirmButton: 'button button--height-44px',
+        cancelButton: 'button button--height-44px button--light-gray-outline',
+        denyButton: 'button button--height-44px button--red',
+        title: 'title title--24px'
+    },
+    buttonsStyling: false
 });
+
+const alertBtn = document.querySelector('#sweet-alert-demo');
+
+if (alertBtn) {
+    alertBtn.addEventListener('click', () => {
+        MySwal.fire({
+            title: "Do you want to save the changes?",
+            icon: "question",
+            showCancelButton: true,
+            showDenyButton: true,
+            confirmButtonText: "Save",
+            denyButtonText: "Don't save",
+            cancelButtonText: "Cancel"
+        }).then((result) => {
+            /* Пример обработки результата */
+            if (result.isConfirmed) {
+                MySwal.fire("Saved!", "", "success");
+            } else if (result.isDenied) {
+                MySwal.fire("Changes are not saved", "", "info");
+            }
+        });
+    });
+}
