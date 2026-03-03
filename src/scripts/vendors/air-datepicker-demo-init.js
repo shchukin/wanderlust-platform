0c64b9fd18889ba7d952ecdc7ae4843462d74d3f
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-03 19:17:02 +0700

    air datepicker

--- src/scripts/vendors/air-datepicker-demo-init.js
+++ src/scripts/vendors/air-datepicker-demo-init.js
@@ -1,26 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
+import AirDatepicker from 'https://cdn.jsdelivr.net/npm/air-datepicker@3.5.0/+esm';
+import localeEn from 'https://cdn.jsdelivr.net/npm/air-datepicker@3.5.0/locale/en/+esm';
 
-    /* If there will be webpack/vite, you can import localeEn like so:
-       import localeEn from 'air-datepicker/locale/en';
-     */
-
-    const localeEn = {
-        days: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-        daysShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-        daysMin: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-        months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
-        monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-        today: 'Today',
-        clear: 'Clear',
-        dateFormat: 'MM/dd/yyyy',
-        timeFormat: 'hh:ii aa',
-        firstDay: 0
-    };
-
-    new AirDatepicker('#date-start', {
-        locale: localeEn,
-        dateFormat: 'MM/dd/yyyy',
-        autoClose: true,
-        buttons: ['today', 'clear']
-    });
+new AirDatepicker('#el', {
+    locale: localeEn.default || localeEn,
+    dateFormat: 'MM/dd/yyyy',
+    autoClose: true,
+    buttons: ['today', 'clear']
 });
