38f3071327e853df1337738d4ecbd417feafc987
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-16 18:56:06 +0700

    cases

---
+++ src/scripts/components/cases.js
@@ -0,0 +1,18 @@
+export const cases = () => {
+    const labels = document.querySelectorAll('[data-cases-target]');
+
+    labels.forEach((label) => {
+        label.addEventListener('click', () => {
+            const targetRef = label.dataset.casesTarget;
+            const targetTab = document.querySelector(`[data-cases-tab-ref="${targetRef}"]`);
+
+            if (targetTab) {
+                document.querySelectorAll('.cases__tab').forEach((tab) => {
+                    tab.classList.remove('cases__tab--current');
+                });
+
+                targetTab.classList.add('cases__tab--current');
+            }
+        });
+    });
+};
