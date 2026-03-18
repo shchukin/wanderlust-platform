b0003f15dd41092e5684e5dbdef6dc30cbf706fe
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-18 15:44:11 +0700

    clean up

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -1,15 +1,10 @@
 export const tabs = () => {
-    const radios = document.querySelectorAll('.tabs__tags input[type="radio"]');
+    const radios = document.querySelectorAll('[data-tab-target]');
 
     radios.forEach((radio) => {
         radio.addEventListener('change', () => {
 
             const targetRef = radio.dataset.tabTarget;
-
-            if (!targetRef) {
-                return;
-            }
-
             const $targetTab = document.querySelector(`[data-tab-body="${targetRef}"]`);
 
             document.querySelectorAll('.tabs__body').forEach((tab) => {
