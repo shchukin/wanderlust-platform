6e0c04e74a539e3d6f20568f5b06de108b1afa8d
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-18 15:41:49 +0700

    clean up

--- src/scripts/components/tabs.js
+++ src/scripts/components/tabs.js
@@ -1,18 +1,23 @@
 export const tabs = () => {
-    const labels = document.querySelectorAll('.tabs .checkboxes-menu__item label');
+    const radios = document.querySelectorAll('.tabs__tags input[type="radio"]');
 
-    labels.forEach((label) => {
-        label.addEventListener('click', () => {
-            const targetRef = label.dataset.tabTarget;
+    radios.forEach((radio) => {
+        radio.addEventListener('change', () => {
 
-            const targetTab = document.querySelector(`[data-tab-body="${targetRef}"]`);
+            const targetRef = radio.dataset.tabTarget;
+
+            if (!targetRef) {
+                return;
+            }
+
+            const $targetTab = document.querySelector(`[data-tab-body="${targetRef}"]`);
 
             document.querySelectorAll('.tabs__body').forEach((tab) => {
                 tab.classList.remove('tabs__body--current');
             });
 
-            if (targetTab) {
-                targetTab.classList.add('tabs__body--current');
+            if ($targetTab) {
+                $targetTab.classList.add('tabs__body--current');
             }
         });
     });
