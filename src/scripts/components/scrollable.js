2b3f768e19c43ee797f0f34583fdea28e1756788
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-28 17:53:24 +0700

    resize

--- src/scripts/components/scrollable.js
+++ src/scripts/components/scrollable.js
@@ -2,32 +2,45 @@ export const scrollable = () => {
     const $scrollablesAll = document.querySelectorAll('.scrollable');
 
     $scrollablesAll.forEach(scrollable => {
-        const prev = scrollable.querySelector('.scrollable__action--prev');
-        const next = scrollable.querySelector('.scrollable__action--next');
-        const body = scrollable.querySelector('.scrollable__body');
-        const ribbon = scrollable.querySelector('.scrollable__ribbon');
+        const $prev = scrollable.querySelector('.scrollable__action--prev');
+        const $next = scrollable.querySelector('.scrollable__action--next');
+        const $body = scrollable.querySelector('.scrollable__body');
+        const $ribbon = scrollable.querySelector('.scrollable__ribbon');
 
-        const isScrollable = ribbon.offsetWidth > body.offsetWidth + 10; /* 10px is some sort of threshold. Don't init if there is no real need for that */
-
-        if (!isScrollable) {
-            return;
-        }
-
-        scrollable.classList.add('scrollable--initialized');
-
-        prev.addEventListener('click', () => {
-            body.scrollTo({
+        const onPrev = () => {
+            $body.scrollTo({
                 left: 0,
                 behavior: 'smooth'
             });
-        });
+        };
 
-        next.addEventListener('click', () => {
-            body.scrollTo({
-                left: body.scrollWidth,
+        const onNext = () => {
+            $body.scrollTo({
+                left: $body.scrollWidth,
                 behavior: 'smooth'
             });
-        });
+        };
 
+        const update = () => {
+            const isScrollable = $ribbon.offsetWidth > $body.offsetWidth + 10; /* 10px is some sort of threshold. Don't init if there is no real need for that */
+
+            if (isScrollable) {
+                if (!scrollable.classList.contains('scrollable--initialized')) {
+                    scrollable.classList.add('scrollable--initialized');
+                    $prev.addEventListener('click', onPrev);
+                    $next.addEventListener('click', onNext);
+                }
+                return;
+            }
+
+            if (scrollable.classList.contains('scrollable--initialized')) {
+                scrollable.classList.remove('scrollable--initialized');
+                $prev.removeEventListener('click', onPrev);
+                $next.removeEventListener('click', onNext);
+            }
+        };
+
+        update();
+        window.addEventListener('resize', update);
     });
 };
