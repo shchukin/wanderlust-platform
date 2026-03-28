4799d3a08bdd957c9e9579ec816765d9d310b5ff
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-28 18:18:21 +0700

    scrolled

--- src/scripts/components/scrollable.js
+++ src/scripts/components/scrollable.js
@@ -7,6 +7,16 @@ export const scrollable = () => {
         const $body = scrollable.querySelector('.scrollable__body');
         const $ribbon = scrollable.querySelector('.scrollable__ribbon');
 
+        const updateScrollState = () => {
+            const edgeThreshold = 10;
+            const maxScrollLeft = $body.scrollWidth - $body.clientWidth;
+            const atLeft = $body.scrollLeft <= edgeThreshold;
+            const atRight = $body.scrollLeft >= maxScrollLeft - edgeThreshold;
+
+            scrollable.classList.toggle('scrollable--scrolled-to-left', atLeft);
+            scrollable.classList.toggle('scrollable--scrolled-to-right', atRight);
+        };
+
         const onPrev = () => {
             $body.scrollTo({
                 left: 0,
@@ -21,6 +31,10 @@ export const scrollable = () => {
             });
         };
 
+        const onScroll = () => {
+            updateScrollState();
+        };
+
         const update = () => {
             const isScrollable = $ribbon.offsetWidth > $body.offsetWidth + 10; /* 10px is some sort of threshold. Don't init if there is no real need for that */
 
@@ -29,7 +43,9 @@ export const scrollable = () => {
                     scrollable.classList.add('scrollable--initialized');
                     $prev.addEventListener('click', onPrev);
                     $next.addEventListener('click', onNext);
+                    $body.addEventListener('scroll', onScroll, { passive: true });
                 }
+                updateScrollState();
                 return;
             }
 
@@ -37,6 +53,8 @@ export const scrollable = () => {
                 scrollable.classList.remove('scrollable--initialized');
                 $prev.removeEventListener('click', onPrev);
                 $next.removeEventListener('click', onNext);
+                $body.removeEventListener('scroll', onScroll, { passive: true });
+                scrollable.classList.remove('scrollable--scrolled-to-left', 'scrollable--scrolled-to-right');
             }
         };
 
