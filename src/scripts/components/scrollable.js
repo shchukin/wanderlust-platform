344784acbc5ab6ca20afeb57dae87b5796a267f8
Anton Shchukin <a.a.shchukin@gmail.com>
2026-03-28 17:01:46 +0700

    script draft

--- src/scripts/components/scrollable.js
+++ src/scripts/components/scrollable.js
@@ -1,3 +1,36 @@
 export const scrollable = () => {
-    
+    const $scrollablesAll = document.querySelectorAll('.scrollable');
+
+    $scrollablesAll.forEach(scrollable => {
+        const prev = scrollable.querySelector('.scrollable__action--prev');
+        const next = scrollable.querySelector('.scrollable__action--next');
+        const body = scrollable.querySelector('.scrollable__body');
+        const ribbon = scrollable.querySelector('.scrollable__ribbon');
+
+        const isScrollable = ribbon.offsetWidth > body.offsetWidth + 10; /* 10px is some sort of threshold. Don't init if there is no real need for that */
+
+        if (!isScrollable) {
+            return;
+        }
+
+        const actions = scrollable.querySelectorAll('.scrollable__action');
+        actions.forEach(action => {
+            action.style.display = 'block';
+        });
+
+        prev.addEventListener('click', () => {
+            body.scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+        });
+
+        next.addEventListener('click', () => {
+            body.scrollTo({
+                left: body.scrollWidth,
+                behavior: 'smooth'
+            });
+        });
+
+    });
 };
