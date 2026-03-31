791ba168506647653f7a6aeca00b64e4d81dead2
Anton Shchukin <a.a.shchukin@gmail.com>
2026-04-01 03:42:24 +0700

    refresh in hyst

--- src/scripts/vendors/hyst-modal-demo-init.js
+++ src/scripts/vendors/hyst-modal-demo-init.js
@@ -1,3 +1,11 @@
+import { refreshExpandableTextareas } from '../components/input.js';
+
 new HystModal({
     linkAttributeName: "data-hystmodal",
+    beforeOpen: (modal) => {
+        const root = modal?.openedWindow || modal?.modal || document;
+        requestAnimationFrame(() => {
+            refreshExpandableTextareas(root);
+        });
+    },
 });
