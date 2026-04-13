f5b3abcc5a0fafe2158b768419bbd242b60a1028
Anton Shchukin <a.a.shchukin@gmail.com>
2026-04-13 20:14:43 +0700

    nav clean up

--- src/scripts/components/nav.js
+++ src/scripts/components/nav.js
@@ -1,20 +1,11 @@
 export const nav = () => {
 
-    const $nav = document.querySelector('.nav');
-
     document.addEventListener('click', (event) => {
 
-        /* Root link (clicking it expands a section) */
+        /* Expand dropdown */
         if (event.target.closest('.nav__section:has(.nav__sub-menu) > .nav__link')) {
             event.preventDefault();
             event.target.closest('.nav__section').classList.toggle('nav__section--expanded');
-        }
-
-        /* Any other link (clicking it makes it current) */
-        if (event.target.closest('.nav__link:not(.nav__section:has(.nav__sub-menu) > .nav__link)')) {
-            event.preventDefault();
-            $nav.querySelector('.nav__link--current').classList.remove('nav__link--current');
-            event.target.closest('.nav__link').classList.add('nav__link--current');
         }
     });
 
